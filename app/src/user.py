d6fed64c7aa59e6acade662001e5cf95c2ad0d00
Slava <slavik3001@rambler.ru>
2024-10-04 01:02:15 +0300

    added some methods

--- app/src/user.py
+++ app/src/user.py
@@ -6,3 +6,17 @@ class User:
         self.name = name
         self.id = 0
         self._balance = balance
+
+    def __repr__(self):
+        return f'User {self.id}. Name: {self.name}'
+
+    def get_balance(self):
+        return self._balance
+
+    def less_balance(self, count):
+        if self.get_balance() - count < 0:
+            raise ValueError('No enough money on the balance')
+        self._balance -= count
+
+    def increase_balance(self, count):
+        self._balance += count
